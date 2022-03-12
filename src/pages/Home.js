02f4503a4a168fb08b1fb1f87f27f5f8fb49f527
Elisa <elisacanyelles@gmail.com>
2022-03-12 16:42:13 +0100

    created components

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroPage from '../components/HeroPage'
+import Quote from '../components/Quote'
+import IntroResto from '../components/IntroResto'
+
+const Home = () => {
+  return (
+    <>
+      <HeroPage />
+      <Quote />
+      <IntroResto/>
+    </>
+  )
+}
+
+export default Home
