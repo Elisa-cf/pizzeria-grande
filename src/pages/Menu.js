ec1bb549ad85daf0fd9072c405e8938b09fa4d28
Elisa <elisacanyelles@gmail.com>
2022-03-17 00:47:55 +0100

    created menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import NavBar from '../components/NavBar';
+import NewsLetter from '../components/NewsLetter';
+import MenuItems from '../components/MenuItems';
+import MenuCategories from '../components/MenuCategories';
+import items from '../assets/data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]; 
+console.log(allCategories);
 
-const Menu = () => {
+function Menu({ handleAddProduct, handleRemoveProduct, cartItems, setCartItems }) {
+  const [categories, setCategories] = useState(allCategories);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
   return (
-    <div>Menu</div>
-  )
+    <main>
+      <NavBar />
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <MenuCategories
+          setSelectedCategory={setSelectedCategory}
+          categories={allCategories}
+        />
+        <MenuItems
+          cartItems={cartItems}
+          handleRemoveProduct={handleRemoveProduct}
+          handleAddProduct={handleAddProduct}
+          selectedCategory={selectedCategory}
+        />
+      </section>
+      <NewsLetter />
+    </main>
+  );
 }
 
 export default Menu
