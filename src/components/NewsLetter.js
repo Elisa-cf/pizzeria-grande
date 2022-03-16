95d7c974fba1e4f17de246f4ebc1ee5b89c01749
Elisa <elisacanyelles@gmail.com>
2022-03-17 00:43:28 +0100

    created functional newsletter using formspree

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,88 +1,89 @@
-import React from 'react'
 import styled from 'styled-components'
 
 const NewsLetter = () => {
- return <Wrapper><div className="section-center">
-  <h2>Join our newsletter. Exclusive offers!</h2>
-  <div className="content">
-   <p>Become a premium customer! Be the first to know about our discounts, promotions and unique surprises.</p>
-   <form className="contact-form"
-    action="https://formspree.io/f/myyokagy"
-    method="POST"
-   >
-    <input type="email" className='form-input' placeholder="enter email"
-     name="_replyto" />
-    <button type="submit" className='btn submit-btn'>subscribe</button>
-   </form>
-  </div>
- </div></Wrapper>
+  return <Wrapper>
+    <h2>Join our newsletter. Exclusive offers!</h2>
+    <div className="content">
+      <p>Become a premium customer! Be the first to know about our discounts, promotions and unique surprises.</p>
+      <form className="contact-form"
+        action="https://formspree.io/f/myyokagy"
+        method="POST"
+      >
+        <input type="email" className='form-input' placeholder="enter email"
+          name="_replyto" />
+        <button type="submit" className='submit-btn'>subscribe</button>
+      </form>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
-  padding-bottom: 2rem;
-  h2 {
-    text-transform: none;
+width: 90vw;
+margin: 0 auto;
+max-width: 1170px;
+padding: 2rem 0;
+h2 {
+  text-transform: none;
+}
+p {
+  max-width: 45em;
+  margin-bottom: 1rem;
+}
+.contact-form {
+  max-width: 500px;
+  display: grid;
+  grid-template-columns: 1fr auto;
+}
+.form-input,
+.submit-btn{
+  font-size: 1rem;
+  padding: 0.3rem 0.2rem;
+  border: 2px solid #0b1015;
+  }
+.form-input {
+  border-right: none;
+  color: #0b1015;
+  border-top-left-radius: 0.25rem;;
+  border-bottom-left-radius: 0.25rem;;
+  }
+.form-input::placeholder {
+  color: #0b1015;
+  text-transform: capitalize;
+}
+.submit-btn {
+  border-top-right-radius: 0.25rem;;
+  border-bottom-right-radius: 0.25rem;
+  font-family: 'Cormorant Upright', serif;
+  font-weight: bold;
+  background: #03c03c;
+  text-transform: uppercase;
+  letter-spacing:  0.1rem;;
+  cursor: pointer;
+  transition: all 0.3s linear;
+  color: white;
+  font-weight: bold;
+}
+.submit-btn:hover {
+  background-color: #ffc500;
+  color: #0b1015;
+}
+@media (min-width: 900px) {
+  .content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 8rem;
+    margin-top: 2rem;
   }
   p {
-    max-width: 45em;
+    margin-bottom: 0;
   }
-  .contact-form {
-    max-width: 500px;
-    display: grid;
-    grid-template-columns: 1fr auto;
-  }
-
   .form-input,
   .submit-btn {
-    font-size: 1rem;
-    padding: 0.3rem 0.2rem;
-    border: 2px solid #0b1015;
-  }
-  .form-input {
-    border-right: none;
-    color: #0b1015;
-    border-top-left-radius: 0.25rem;;
-    border-bottom-left-radius: 0.25rem;;
-  }
-  .form-input::placeholder {
-    color: #0b1015;
-    text-transform: capitalize;
-  }
-  .submit-btn {
-    border-top-right-radius: 0.25rem;;
-    border-bottom-right-radius: 0.25rem;
-    font-family: 'Cormorant Upright', serif;
-    font-weight: bold;
-    background: #03c03c;
-    text-transform: uppercase;
-    letter-spacing:  0.1rem;;
-    cursor: pointer;
-    transition: all 0.3s linear;
-    color: white;
-    font-weight: bold;
-  }
-  .submit-btn:hover {
-   background-color: #ffc500;
-   color: #0b1015;
-  }
-  @media (min-width: 900px) {
-    .content {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      align-items: center;
-      gap: 8rem;
-      margin-top: 2rem;
-    }
-    p {
-      margin-bottom: 0;
-    }
-    .form-input,
-    .submit-btn {
     padding: 0.5rem 1rem;
-    }
-     @media (min-width: 1280px) {
+  }
+  @media (min-width: 1280px) {
     padding: 3rem 0;
   }
-  }
+}
 `
-
 export default NewsLetter
