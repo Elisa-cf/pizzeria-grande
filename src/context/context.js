92bd4aa5ff185d1d69ece87e995524d9fa4c861a
Elisa <elisacanyelles@gmail.com>
2022-03-17 00:44:24 +0100

    created context for sidebar

---
+++ src/context/context.js
@@ -0,0 +1,36 @@
+import React, { useState, createContext } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+ const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+ const openSidebar = () => {
+  setIsSidebarOpen(true);
+ };
+
+ const closeSidebar = () => {
+  setIsSidebarOpen(false);
+ };
+
+ return (
+  <AppContext.Provider
+   value={{
+    isSidebarOpen,
+    openSidebar,
+    closeSidebar,
+   }}
+  >
+   {children}
+  </AppContext.Provider>
+ );
+};
+
+export default AppContext;
+export { AppProvider };
+
+// export const useGlobalContext = () => {
+//  return useContext(AppContext);
+// };
+
+// export { AppContext, AppProvider };
