01d130d98f8dc15ef94d817e1e03fc897a41d25d
Elisa <elisacanyelles@gmail.com>
2022-03-17 00:32:17 +0100

    uncommented context provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+// import { AppProvider } from './context/context';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      {/* <AppProvider> */}
+        <App />
+      {/* </AppProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
